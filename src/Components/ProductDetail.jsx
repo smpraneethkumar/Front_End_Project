d4fa0150eb29737b3ed3b905ba9d1cf40006aabc
sm praneeth kumar <smparneethkumr431gmail.com>
2025-08-20 10:22:14 +0530

    creating product details page

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -1,54 +1,54 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { discoutProducts } from './AllData';
-
+import { FaStar } from "react-icons/fa6";
 function ProductDetail() {
 
   const {id} = useParams()
-  console.log(id);
-  console.log(discoutProducts) 
+  // console.log(id);
+  // console.log(discoutProducts) 
 
   const [productName] = discoutProducts.filter((ele)=>
      ele.id === id
 
 
 )
-  console.log(productName);
+let [{rating ,text}] = productName.reviews
+console.log(rating);
+
+// console.log(productName.reviews);
+
+  // console.log("rrfrkropfker"+productName.reviews.rating);
+
   
   return (
     <div>
       <h1>ProductDetail</h1>
-      <h2>{productName.productName      }</h2>
-      
-        {/* <h1>{file.id}</h1> */}
-      
+      <div className="container">
+        <div className="row">
+          <div className='col-6'>
+              <img src={productName.imgUrl} alt="" />
+          </div>
+          <div className='col-6'>
+            <h2>{productName.productName   }</h2>
+            <span className='d-flex gap-4'>
 
-{/* {
-     discoutProducts.id && file.map((e, i)=>{
-    <h1 key={i}>{e.price}</h1>
+             <p className="card-text"><FaStar color='gold' /><FaStar color='gold' /><FaStar color='gold' /><FaStar color='gold' /><FaStar color='gold' /></p>
+             {/* <h5>{productName.}</h5>/ */}
+             <h5>{rating} rating</h5>
+            </span><br />
+            <span className='d-flex gap-5'>
 
-  })
-} */}
+            <h4>${productName.price}</h4>
+            <h5>category:{productName.category}</h5>
+            </span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit labore a fugiat voluptatibus nemo minus dolorum rerum quasi, </p>
+             <button className='btn btn-outline- btn-success'>Add to Cart</button>
 
-
-
-
-
-{/* /* if (`{id} === {discoutProducts}`) {
-  
-  <h1>{id}</h1> */}
-  
-
-
-       {/* {
-        discoutProducts.id && id.map((id)=>{
-
-          <h1>{id}</h1>
-          // <h1>{idd.productName}</h1>
-          // <img src={d.imgUrl} alt="" />
-        })
-      } */}
-      <h2>fgfg</h2>
+          </div>
+        </div>
+      </div>
+     
     </div>
   )
 }
