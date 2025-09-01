d8770f99d457d0027c02781e446fe41d66b2e2c2
sm praneeth kumar <smparneethkumr431gmail.com>
2025-09-01 18:01:35 +0530

    create redux

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -4,57 +4,63 @@ import { products } from './AllData';
 import table from "../assets/table.jpg"
 import { FaStar } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
 
 function ProductDetail() {
+  const dispatch = useDispatch()
 
   const { id } = useParams()
   // console.log(id);
   // console.log(discoutProducts) 
 
-  const [productName] = products.filter((ele) =>
+  const [singlePro] = products.filter((ele) =>
     ele.id === id
 
 
   )
-  let [{ rating  ,text}] = productName.reviews
+  let [{ rating  ,text}] = singlePro.reviews
   // console.log(rating);
 
-  // console.log(productName.reviews);
+  // console.log(singlePro.reviews);
 
-  // console.log("rrfrkropfker"+productName.reviews.rating);
+  // console.log("rrfrkropfker"+singlePro.reviews.rating);
 
   // function desc (){
   //    return  `<p>sdfdsfdsfsdsdfsdfsfs</p>`
   // }
 
+  const handleAdd =(singlePro)=>{
+    dispatch
+  }
   return (
     <div>
       <h1>ProductDetail</h1>
       <div >
 
       <img src={table} alt="" style={{height:"250px",width:"100%"}} />
-      {/* <h4  style={{color:"white"}}>{productName.productName}</h4> */}
+      {/* <h4  style={{color:"white"}}>{singlePro.singlePro}</h4> */}
       <div className="container">
         <div className="row">
           <div className='col-6' >
-            <img src={productName.imgUrl} alt="" style={{width:"100%",height:"80%"}} />
+            <img src={singlePro.imgUrl} alt="" style={{width:"100%",height:"80%"}} />
           </div>
           <div className='col-6'>
-            <h2>{productName.productName}</h2>
+            <h2>{singlePro.singlePro}</h2>
             <span className='d-flex gap-4'>
 
               <p className="card-text"><FaStar color='gold' /><FaStar color='gold' /><FaStar color='gold' /><FaStar color='gold' /><FaStar color='gold' /></p>
-              {/* <h5>{productName.}</h5>/ */}
+              {/* <h5>{singlePro.}</h5>/ */}
               <h5>{rating} rating</h5>
             </span><br />
             <span className='d-flex gap-5'>
 
-              <h4>${productName.price}</h4>
-              <h5>category:{productName.category}</h5>
+              <h4>${singlePro.price}</h4>
+              <h5>category:{singlePro.category}</h5>
             </span>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odit labore a fugiat voluptatibus nemo minus dolorum rerum quasi, </p>
             <input type="number" style={{width:"80px"}} id=""placeholder='1'  className='mt-3'/> <br/> 
-            <Link to={`/pDetail/${productName.id}`}><button className='btn btn-outline- btn-success mt-5'>Add to Cart</button></Link>
+            <button className='btn btn-outline- btn-success mt-5'onClick={()=>{handleAdd(singlePro)}} >Add to Cart</button>
 
           </div>
         </div>
