39ae048addaa2b3078e00bf37ba04550effadb0b
sm praneeth kumar <smparneethkumr431gmail.com>
2025-09-02 16:01:53 +0530

    working on cart ui page

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -2,50 +2,99 @@
 import { useParams } from 'react-router-dom'
 import { discoutProducts } from './AllData';
 import { useSelector } from 'react-redux';
+import { FaPlus } from "react-icons/fa6";
+import { FaMinus } from "react-icons/fa6";
 // import { discoutProducts } from './AllData';
 
 
 const Cart = () => {
 
   const cartData = useSelector(state=> state.cart)
+  // console.log(cartData);
   console.log(cartData);
   
-  const {id} = useParams()
-  console.log(id);
 
-  const [cartProduct] = discoutProducts.filter((ele)=>
-  ele.id=== id
-    )
-    console.log(cartProduct);
+  // const {id} = useParams()
+  // console.log(id);
 
-  0
+  // const [cartProduct] = discoutProducts.filter((ele)=>
+  // ele.id=== id
+  //   )
+  //   console.log(cartProduct);
+
+  
     
     
 
   return (
-    <div>
-        <h2>Cart</h2>
+    <div className='container'>
+      <div className="row">
+        <div className="col-md-9 ">
+         {
+          cartData && cartData.map((cart)=>(
+           
+         
+             <div className='d-flex gap-5'>
+               <img style={{width:"20%",height:"40%"}} src={cart.imgUrl} alt="" />
+               <h5>{cart.productName}</h5>
+               <p>${cart.price} * 3 ${cart.price}</p>
 
-        {
-          // cartProduct 
-          cartProduct > 0 ? 
-            <h2> your cart is empty</h2> :
-            
-        <div className="container">
-          <div className="row gap-5">
-            <div className='col-7 '>
-              <img style={{width:"30%",height:"50%"}} src={cartProduct.imgUrl} alt="" />
-              </div>
-              <div className="col-4">
-                <h4 className='display-6'>Cart Summary</h4>
-                <hr />
-                <b>Total Price:353</b>
-              </div>
-              </div>
-              </div>
-       
+               
+                <div>
+                <button type="button" class="btn btn-outline-primary btn-sm me  5"><FaPlus /></button>
+               <button type="button" class="btn btn-outline-primary btn-sm h-0 w-0"><FaMinus/></button> 
+
+                </div>
+                   </div>
+              
+             
+             
+          ))  
         }
 
+        </div>
+        <div className="col-md-3">
+    
+                 <h4 className='display-6'>Cart Summary</h4>
+                 <hr />
+                 <b>Total Price:353</b>
+               </div>
+       
+      </div>
+        <h2>Cart</h2>
+
+
+
+       
+        {/* <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+          <h3>sdfsfsdfs</h3>
+            </div>
+            <div className="col-md-3">
+              <h2>sflkdfsldkfjsdklfjds</h2>
+            </div>
+          </div>
+         </div>     
+        */}
+        
+          {/* // cartProduct 
+          // cartProduct < 0 ? 
+          
+            
+        // <div className="container">
+        //   <div className="row gap-5">
+        //     <div className='col-7 '>
+        //       <img style={{width:"30%",height:"50%"}} src={cartProduct.imgUrl} alt="" />
+        //       </div>
+        //       <div className="col-4">
+        //         <h4 className='display-6'>Cart Summary</h4>
+        //         <hr />
+        //         <b>Total Price:353</b>
+        //       </div>
+        //       </div>
+        //       </div>
+              //  : <h2> your cart is empty</h2> */}
 
     </div>
   )
--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -12,9 +12,9 @@ export class Routing extends Component {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/shop' element={<Shop/>}/>
-        {/* <Route path='/cart' element={<Cart/>}/> */}
+        <Route path='/cart' element={<Cart/>}/>
         <Route path='/home/:id' element={<ProductDetail/>}/>
-        <Route path='/pDetail/:id' element={<Cart/>}/>
+        {/* <Route path='/pDetail/:id' element={<Cart/>}/> */}
         
       
       </Routes>
