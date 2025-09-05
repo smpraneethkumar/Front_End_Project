9acfb4dc4cba4afbf8cc0b27548af84fabfdccd9
sm praneeth kumar <smparneethkumr431gmail.com>
2025-09-05 18:40:55 +0530

    did on fliter category and search input

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -2,44 +2,79 @@ import React, { useState } from 'react'
 import { products } from './AllData';
 import { FaStar } from 'react-icons/fa';
 
-
-
 const Shop = () => {
- 
+  const [selectCategory, setSelectCategory] = useState("all");
+  const [search, setSearch]= useState("")
+  // Filtering logic
+  // console.log(search);
+  
+  //  if(search.length==products.category){
+    
+  //  } 
+  
+  const filterProducts = products.filter((item)=>{
+    const drop = selectCategory === "all" ||  item.category === selectCategory;
+
+    const searchData =  item.category.toLowerCase().includes(search.toLowerCase());  
+
+    return  searchData && drop
+
+  })
+   
   return (
     <div>
       <div className='d-flex justify-content-between  m-3'>
-        <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Filter By Category
-  </button>
-  <ul class="dropdown-menu">y
-    <li><a class="dropdown-item" href="#">sofa</a></li>
-    <li><a class="dropdown-item" href="#">chair</a></li>
-    <li><a class="dropdown-item" href="#">watch</a></li>
-    <li><a class="dropdown-item" href="#">mobile</a></li>
-    <li><a class="dropdown-item" href="#">wireless</a></li>
-  </ul>
-</div>
+        
+        {/* Dropdown */}
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Filter By Category
+          </button>
+          <ul className="dropdown-menu">
+            <li><button className="dropdown-item" onClick={() => setSelectCategory("all")}>All</button></li>
+            <li><button className="dropdown-item" onClick={() => setSelectCategory("sofa")}>Sofa</button></li>
+            <li><button className="dropdown-item" onClick={() => setSelectCategory("chair")}>Chair</button></li>
+            <li><button className="dropdown-item" onClick={() =>setSelectCategory("watch")}>Watch</button></li>
+            <li><button className="dropdown-item" onClick={() => setSelectCategory("mobile")}>Mobile</button></li>
+            <li><button className="dropdown-item" onClick={()=> setSelectCategory("wireless")}>Wireless</button></li>
+          </ul>
+        </div>
+
+    
         <div className="col-sm-5">
-    <input type="text" className="form-control" placeholder="Search" aria-label="City"/>
-  </div>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search"
+            aria-label="Search"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+        </div>
       </div>
+
+      {/* Products */}
       <div className="container">
         <div className="row row-cols-1 row-cols-md-3 g-4">
-          {
-            products && products.map((allPro) => (
-              <div className="col" key={allPro.id}>
-                <div className="card">
-                  <img src={allPro.imgUrl} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{allPro.productName}</h5>
-                    <p className="card-text"><FaStar color='gold' /><FaStar color='gold' /><FaStar color='gold' /><FaStar color='gold' /><FaStar color='gold' /></p>
-                    <h2 className="card-text">{allPro.price}</h2>
-                  </div>
+          { 
+          filterProducts.length > 0 ? filterProducts.map((item) => (
+            <div className="col" key={item.id}>
+              <div className="card">
+                <img src={item.imgUrl} className="card-img-top" alt={item.productName} />
+                <div className="card-body">
+                  <h5 className="card-title">{item.productName}</h5>
+                  <p className="card-text">
+                    <FaStar color="gold" />
+                    <FaStar color="gold" />
+                    <FaStar color="gold" />
+                    <FaStar color="gold" />
+                    <FaStar color="gold" />
+                  </p>
+                  <h2 className="card-text">{item.price}</h2>
                 </div>
               </div>
-            ))
+            </div>
+          )) : <h3 className='display-6  ' style={{textAlign:"center",width:"95%",height:"50%"}}>No Products are found</h3>
           }
         </div>
       </div>
